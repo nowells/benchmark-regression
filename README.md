aaaef2f4f41e0e4cae1bd63778322b59daa55a89
Nowell Strite <nowell@strite.org>
2018-09-22 00:49:06 -0400

    Updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # `@clevernature/benchmark-regression`
 
 Generates performance regression tests using benchmarkjs.
+
+## Code
+
+```js
+const createRegressionBenchmark = require('@clevernature/benchmark-regression');
+const currentClient = require('..');
+
+const benchmarks = createRegressionBenchmark(currentClient, ['prom-client@11.1.2', 'prom-client@11.1.1']);
+
+benchmarks.suite('registry', (suite) => {
+    suite.add(
+        'getMetricsAsJSON',
+        (client, { registry }) => registry.getMetricsAsJSON(),
+        { setup }
+    );
+    suite.add(
+        'metrics',
+        (client, { registry }) => registry.metrics(),
+        { setup }
+    );
+});
+
+benchmarks.add(
+    'histogram#observe',
+    (client, { histogram }) => histogram.observe(1, { a: 1, b: 1 }),
+    { setup }
+);
+
+benchmarks.run().catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+});
+
+function setup(client) {
+    const registry = new client.Registry();
+
+    const histogram = new client.Histogram({
+        name: 'histogram',
+        help: 'histogram',
+        labelNames: ['a', 'b'],
+        registers: [registry]
+    });
+
+    histogram.observe(1, { a: 1, b: 1 });
+
+    return {registry, histogram};
+}
+```
+
+## Results
+
+![Benchmark Results](./assets/results.png)
--- test/index.js
+++ test/index.js
@@ -2,16 +2,43 @@
 
 const createRegressionBenchmark = require('..');
 
-const benchmarks = createRegressionBenchmark({name: 'latest', module: 'prom-client'}, [
-    'prom-client@11.1.2',
-    'prom-client@11.1.1',
-    'prom-client@11.1.0'
-]);
+const benchmarks = createRegressionBenchmark({name: 'latest', module: 'prom-client'}, ['prom-client@11.1.2', 'prom-client@11.1.1']);
 
-benchmarks.suite('registry', require('./registry'));
-benchmarks.suite('histogram', require('./histogram'));
+benchmarks.suite('registry', (suite) => {
+    suite.add(
+        'getMetricsAsJSON',
+        (client, { registry }) => registry.getMetricsAsJSON(),
+        { setup }
+    );
+    suite.add(
+        'metrics',
+        (client, { registry }) => registry.metrics(),
+        { setup }
+    );
+});
+
+benchmarks.add(
+    'histogram#observe',
+    (client, { histogram }) => histogram.observe(1, { a: 1, b: 1 }),
+    { setup }
+);
+
 benchmarks.run().catch(err => {
     console.error(err.stack);
-    // eslint-disable-next-line no-process-exit
     process.exit(1);
 });
+
+function setup(client) {
+    const registry = new client.Registry();
+
+    const histogram = new client.Histogram({
+        name: 'histogram',
+        help: 'histogram',
+        labelNames: ['a', 'b'],
+        registers: [registry]
+    });
+
+    histogram.observe(1, { a: 1, b: 1 });
+
+    return {registry, histogram};
+}
